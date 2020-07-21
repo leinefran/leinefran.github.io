295463274a42e0d33d7bcb219bf72a9c2f4f0e9b
Leine Francisca Valente <cozymachete@Leines-MacBook-Pro.local>
2020-07-21 16:59:41 -0700

    File containing JQuery code to animate the page elements.

--- main.js
+++ main.js
@@ -3,4 +3,40 @@ $(document).ready(function () {
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
   });
+
+  $('.top-nav .nav-link').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  });
+
+  /* scrolling */
+  /* 1st line: select all the internal links */
+  /* 2nd line: select html and body, and animated it. */
+  $('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate([
+      /* keyframes */
+      { scrollTop: $($(this).attr('href')).offset().top - 100 }
+    ], {
+      /* timing options */
+      duration: 2000,
+      iterations: Infinity });
+  }); 
+
+  /* animate html and body */
+  $('#up').on('click', function () {
+    $('html, body').animate([
+    /* keyframes */
+    { scrollTop: 0 }
+  ], {
+    /* timing options */
+    duration: 2000,
+    iterations: Infinity });
+  });
+
+  /* AOS animate on scroll library */
+  AOS.init({
+    easing: 'ease',
+    duration: 1800,
+    once: true,
+  });
 });
